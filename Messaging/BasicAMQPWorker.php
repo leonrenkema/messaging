ab6a7da43dce5fae90248f0b2415429bbae468b7
Jeroen van Dijk <j.vandijk@syntux.nl>
2013-03-26 12:29:11 +0100

    - Added a QoS implementation with default values if a worker has not implemented the needed QoS args

--- Messaging/BasicAMQPWorker.php
+++ Messaging/BasicAMQPWorker.php
@@ -12,6 +12,12 @@ class BasicAMQPWorker extends Worker {
     
     private $channel;
     
+    //The default prefetch size for workers that have not implemented their own
+    const DEFAULT_PREFETCH_SIZE = 0;
+    
+    //The default prefetch size for workers that have not implemented their own
+    const DEFAULT_PREFETCH_COUNT = 0;
+    
     /**
      * Blocking call that fires the Task every time a message comes in.
      */
@@ -23,20 +29,44 @@ class BasicAMQPWorker extends Worker {
     
     public function registerChannel($name, $handlerClassName) {
         
+        //set the default prefetch args
+        $prefetchSize  = self::DEFAULT_PREFETCH_SIZE;
+        $prefetchCount = self::DEFAULT_PREFETCH_COUNT;
+        
         if ($this->connection == null) {
             $this->prepareConnection();
         }
         
+        //check if the class name which is called here has implemented its own prefetch size
+        if(defined($handlerClassName::PREFETCH_SIZE)){
+           $prefetchSize = $handlerClassName::PREFETCH_SIZE;
+        }
+        
+        //check if the class name which is called here has implemented its own prefetch count
+        if(defined($handlerClassName::PREFETCH_COUNT)){
+            $prefetchCount = $handlerClassName::PREFETCH_COUNT;
+        }
+        
+        $this->setQos($prefetchSize, $prefetchCount);
         $this->channel->basic_consume($name, '', false, false, false, false, array($handlerClassName, 'execute'));
     }
     
+    /**
+     * Set the QoS for a channel. The last param of the basic_qos function is not implemented so we omit it here and set it to false by default.
+     * By doing this we will need to set it for every connection tho
+     * @param unknown_type $prefetchSize
+     * @param unknown_type $prefetchCount
+     */
+    private function setQos($prefetchSize, $prefetchCount){
+    
+        $this->channel->basic_qos($prefetchSize, $prefetchCount, false);
+    }
+     
     protected function prepareConnection() {
         try { 
             $this->connection = new AMQPConnection($this->config['host'], $this->config['port'], $this->config['user'], $this->config['password']);
 
-            $this->channel = $this->connection->channel();
-            $this->channel->basic_qos(0, 10);
-            
+            $this->channel = $this->connection->channel();            
         } catch(AMQPConnectionException $e) {
             return null;
         } catch(AMQPRuntimeException $e2) {
